3cdc615635ba52bc12e655cc5b162dd5c45db245
Mprieto8705 <118488010+Mprieto8705@users.noreply.github.com>
2025-12-06 17:34:51 -0500

    seguridad

--- db.js
+++ db.js
@@ -1,39 +1,21 @@
-/**import mysql from "mysql2";
-
-const db = mysql.createConnection({
- host: "localhost",
- user: "root",
- password: "", // tu contraseña MySQL
-  database: "ecommerce_db"
-});
-
-db.connect(err => {
- if (err) throw err;
- console.log("✅ Conectado a MySQL");
-});
-
-export default db;**/
-
-
 import mysql from "mysql2";
+import fs from "fs";
 
 const db = mysql.createConnection({
-host: "server-bd-app.mysql.database.azure.com",
-user: "mprieto",
-password: "870519Ap/",
-database: "ecommerce_db",
-port: 3306,
-ssl: {
-rejectUnauthorized: true
- }
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: 3306,
+  ssl: { rejectUnauthorized: false } // Opción simple
 });
 
 db.connect(err => {
- if (err) {
-   console.error("❌ Error conectando a Azure MySQL:", err);
-   return;
- }
- console.log("✅ Conectado a Azure MySQL");
+  if (err) {
+    console.error("❌ Error conectando a Azure MySQL:", err);
+    return;
+  }
+  console.log("✅ Conectado a Azure MySQL");
 });
 
 export default db;
