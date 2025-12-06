8d7b7a4e815845ff2fcd5bff0fc1ce9459804323
Mprieto8705 <118488010+Mprieto8705@users.noreply.github.com>
2025-12-06 17:41:31 -0500

    seguridad

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
 import mysql from "mysql2";
-import fs from "fs";
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: 3306,
-  ssl: { rejectUnauthorized: false } // Opción simple
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false }
 });
 
 db.connect(err => {
